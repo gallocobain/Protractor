c3578a51f173538479285a3de2f98a2f573adafd
gallocobain <38699929+gallocobain@users.noreply.github.com>
2019-08-26 22:37:14 -0300

    Add files via upload

--- conf.js
+++ conf.js
@@ -5,9 +5,14 @@ exports.config = {
     framework: 'jasmine2',
     specs: ['specs/*-spec.js'],
     baseUrl: 'https://automacaocombatista.herokuapp.com/',
+
     onPrepare: function () {
         browser.manage().timeouts().implicitlyWait(10000);
         browser.ignoreSynchronization = true;
+
+        TIMEOUT = 30000;
+
+        var EC = protractor.ExpectedCondicions;
 
         var JasmineHtmlReporter = require('protractor-jasmine2-html-reporter');
 
@@ -15,7 +20,7 @@ exports.config = {
             savePath: 'reports',
             screenshotsFolder: './shots',
             takeScreenshots: true,
-            cleanDestination: false,
+            cleanDestination: true,
             fixedScreenshotName: true
         }))
 
@@ -40,11 +45,56 @@ exports.config = {
             }
         }))
 
-        
+
+
+        // var VideoReporter = require('protractor-video-reporter');
+        // jasmine.getEnv().addReporter(new VideoReporter({
+        //     baseDirectory: 'C:\\TesteProtractor\\MarkTask\\reports\\videos',
+        //     saveSuccessVideos: true,
+        //     ffmpegCmd: "D:\\VLC.exe",
+        //     ffmpegArgs: [
+        //         '-f',
+        //         'gdigrab',
+        //         '-framerate',
+        //         '30',
+        //         '-i',
+        //         'video=screen-capture-recorder',
+        //         'title=Google - Mozilla Firefox'
+        //     ],
+
+
+
+        // }));
+
+    },
+    
+    onComplete: function () {
+        var browserName, browserVersion;
+        var capsPromise = browser.getCapabilities();
+
+        capsPromise.then(function (caps) {
+            browserName = caps.get('browserName');
+            browserVersion = caps.get('version');
+            platform = caps.get('platform');
+
+            var HTMLReport = require('protractor-html-reporter-2');
+            testConfig = {
+                reportTitle: 'Protractor Test Execution Report',
+                outputPath: './reports',
+                outputFilename: 'index',
+                screenshotPath: './',
+                testBrowser: browserName,
+                browserVersion: browserVersion,
+                modifiedSuiteName: false,
+                screenshotsOnlyOnFailure: true,
+                testPlatform: platform
+            };
+            new HTMLReport().from('./reports' + '/xml/xmlOutput.xml', testConfig);
+        });
     },
 
     capabilities: {
-        'browserName': 'chrome'
-        //'browserName': 'firefox'
+        //'browserName': 'chrome'
+        'browserName': 'firefox'
     }
 }
