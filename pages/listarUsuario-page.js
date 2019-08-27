47bbab4ffc1362ed1b1cbe1cc2b6fc3f663d50e7
gallocobain <38699929+gallocobain@users.noreply.github.com>
2019-08-26 22:39:29 -0300

    Add files via upload

--- pages/listarUsuario-page.js
+++ pages/listarUsuario-page.js
@@ -1,10 +1,16 @@
 class ListarUsuario {
 
     constructor() {
+        this.EC = protractor.ExpectedCondicions;
         this.path = 'users';
-        this.lista = element(by.css('body > div.row > div > table > tbody > tr:nth-child(1) > td:nth-child(1)'));
+        //this.lista = element(by.css('body > div.row > div > table > tbody > tr:nth-child(1) > td:nth-child(1)'));
+        this.lista = element(by.cssContainingText("tr", 'AAAAA Teste alteração'));
     };
 
+    getItem(lista) {
+        browser.wait(this.EC.presenceOf($('#task-board')), TIMEOUT);
+        return  this.lista;
+    }
 
 };
 
